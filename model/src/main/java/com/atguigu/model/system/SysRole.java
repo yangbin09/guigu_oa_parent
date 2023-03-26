2c1a84bebce6df6d2c442167f9896fd913ccdbff
bin.yang <1692207904@qq.com>
2023-03-26 23:48:08 +0800

    搭建环境

---
+++ model/src/main/java/com/atguigu/model/system/SysRole.java
@@ -0,0 +1,29 @@
+package com.atguigu.model.system;
+
+import com.atguigu.model.base.BaseEntity;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@TableName("sys_role")
+public class SysRole extends BaseEntity {
+
+	private static final long serialVersionUID = 1L;
+
+	//角色名称
+	@TableField("role_name")
+	private String roleName;
+
+	//角色编码
+	@TableField("role_code")
+	private String roleCode;
+
+	//描述
+	@TableField("description")
+	private String description;
+}
+
---
+++ model/src/main/java/com/atguigu/model/system/SysUserRole.java
@@ -0,0 +1,27 @@
+package com.atguigu.model.system;
+
+import com.atguigu.model.base.BaseEntity;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableName;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@ApiModel(description = "用户角色")
+@TableName("sys_user_role")
+public class SysUserRole extends BaseEntity {
+	
+	private static final long serialVersionUID = 1L;
+
+	@ApiModelProperty(value = "角色id")
+	@TableField("role_id")
+	private Long roleId;
+
+	@ApiModelProperty(value = "用户id")
+	@TableField("user_id")
+	private Long userId;
+}
+
